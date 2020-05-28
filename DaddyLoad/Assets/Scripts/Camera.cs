9237e3569608ffc4ba354256368d8673efd1b338
DiscountQuagsire <rayorged@gmail.com>
2020-05-28 22:01:55 +0200

    multiplayer camera

---
+++ DaddyLoad/Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class Camera : MonoBehaviourPunCallbacks
+{
+    public GameObject playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            playerCamera.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
