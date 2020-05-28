eb0ec1cf14a7e15808d858579bae32d213a286d3
BioFrozt <58526137+BioFrozt@users.noreply.github.com>
2020-05-28 21:58:42 +0200

    playermovement
    added

---
+++ DaddyLoad/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviourPunCallbacks
+{
+    // Update is called once per frame
+
+    private void Start()
+    {
+        if (!photonView.IsMine) return;
+    }
+
+    void FixedUpdate()
+    {
+        if (!photonView.IsMine) return;
+        if (Input.GetKey("w")) transform.position += new Vector3(0, 0.1f, 0);
+        if (Input.GetKey("a")) transform.position += new Vector3(-0.1f, 0, 0);
+        if (Input.GetKey("s")) transform.position += new Vector3(0, -0.1f, 0);
+        if (Input.GetKey("d")) transform.position += new Vector3(0.1f, 0, 0);
+    }
+}
