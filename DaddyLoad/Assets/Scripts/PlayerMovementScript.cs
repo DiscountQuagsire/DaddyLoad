864b6d3d1341d1542dc2bf54aa737bde5a43a6af
DiscountQuagsire <rayorged@gmail.com>
2020-05-29 17:48:28 +0200

    BIG UPDATE

--- DaddyLoad/Assets/Scripts/PlayerMovementScript.cs
+++ DaddyLoad/Assets/Scripts/PlayerMovementScript.cs
@@ -6,19 +6,68 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviourPunCallbacks
 {
-    // Update is called once per frame
+    public Animator animator;
+
+    public float flightSpeed;
+    public float runSpeed;
+    private float horizontalMove;
+
+    private bool isJumping = false;
+    private bool isTurnedRight = false;
+
+    private Rigidbody2D rb;
 
     private void Start()
     {
         if (!photonView.IsMine) return;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (!photonView.IsMine) return;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        animator.SetBool("IsJumping", isJumping);
+
+        if (horizontalMove > 0 && !isTurnedRight)
+        {
+            isTurnedRight = !isTurnedRight;
+            Flip();
+        }
+
+        if (horizontalMove<0 && isTurnedRight)
+        {
+            isTurnedRight = !isTurnedRight;
+            Flip();
+        }
+
     }
 
     void FixedUpdate()
     {
         if (!photonView.IsMine) return;
-        if (Input.GetKey("w")) transform.position += new Vector3(0, 0.1f, 0);
-        if (Input.GetKey("a")) transform.position += new Vector3(-0.1f, 0, 0);
+        transform.position += new Vector3(horizontalMove, 0, 0);
+
+
+        if (Input.GetKey("w"))
+        {
+            transform.position += new Vector3(0, flightSpeed, 0);
+            isJumping = true;
+        }
+
+        if (!Input.GetKey("w"))
+        {
+            isJumping = false;
+        }
         if (Input.GetKey("s")) transform.position += new Vector3(0, -0.1f, 0);
-        if (Input.GetKey("d")) transform.position += new Vector3(0.1f, 0, 0);
+
+    }
+
+    private void Flip()
+    {
+   
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
