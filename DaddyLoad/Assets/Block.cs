02ff1b82ae094e81175db8e084fe75c785186eba
DiscountQuagsire <rayorged@gmail.com>
2020-05-30 15:19:23 +0200

    Block script + anim fix

---
+++ DaddyLoad/Assets/Block.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public float maxHealth;
+    private float health;
+    public GameObject deathEffect;
+    private string playerID;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health >= 0)
+        {
+
+            Destroy(this);
+        }
+    }
+
+    private void OnDestroy()
+    {            
+        //Give drops to player with playerID
+        Debug.Log(gameObject.name + " destroyed at" + transform.position);
+    }
+
+    public void takeDamage(float damage, string ID)
+    {
+        playerID = ID;
+        health -= damage;
+    }
+}
