3fd460368ef77d8336bd8be4fab04755325d61b5
DiscountQuagsire <rayorged@gmail.com>
2020-09-07 16:47:23 +0200

    Ship upgrade

---
+++ DaddyLoad/Assets/Scripts/Player Related/ShipUpgrade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipUpgrade : MonoBehaviour
+{
+
+    public GameObject[] Dialogues;
+
+    public void hideAllDialogues()
+    {
+        for (int i=0; i<Dialogues.Length; i++)
+        {
+            Dialogues[i].SetActive(false);
+        }
+    }
+
+    public void showDialogue(int index)
+    {
+        hideAllDialogues();
+        Dialogues[index].SetActive(true);
+    }
+
+}
