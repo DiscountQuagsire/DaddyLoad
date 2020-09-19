86bda5a18657bc0c33eefe4c42a01de40ea75951
BioFrozt <58526137+BioFrozt@users.noreply.github.com>
2020-09-19 12:38:03 +0200

    renamed & uiscript

---
+++ DaddyLoad/Assets/Scripts/Technical/ShipUIScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipUIScript : MonoBehaviour
+{
+
+    public GameObject cockpit;
+    public Sprite cockpit0;
+    public Sprite cockpit1;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            Debug.Log("updating imagew");
+            cockpit.GetComponent<Image>().sprite = cockpit1;
+        }
+    }
+
+
+
+}
