16389ebf9f58816ca5074a860f95e0eacc794f21
Nguyen Thanh Hieu <2401707n@student.gla.ac.uk>
2020-09-26 21:03:42 +0200

    Dialogues

---
+++ DaddyLoad/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable][CreateAssetMenu]
+public class Dialogue : ScriptableObject
+{
+    public string characterName;
+
+    public Sprite sprite;
+
+    [TextArea (3,10)]
+    public string[] sentences;
+
+}
---
+++ DaddyLoad/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject canvas;
+    public Text nameText;
+    public Text dialogueText;
+
+    public Dialogue dialogue;
+    private int currentLine = 0; 
+
+    public void StartDialogue(Dialogue newDialogue)
+    {
+        canvas.SetActive(true);
+        dialogue = newDialogue;
+        nameText.text = dialogue.characterName;
+
+        currentLine = 0;
+        DisplayNextLine();
+    }
+
+    public void DisplayNextLine()
+    {
+        if (currentLine >= dialogue.sentences.Length | dialogue == null)
+        {
+            EndDialogue();
+        }
+        else
+        {
+            dialogueText.text = dialogue.sentences[currentLine];
+            currentLine += 1;
+        }
+
+    }
+
+    public void EndDialogue()
+    {
+        canvas.SetActive(false);
+    }
+
+
+}
+
